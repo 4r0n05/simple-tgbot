dac1cc00a16f192e639cd55e2fc92fa04a4cc4d5
dnoggg <dnoggg@openmailbox.org>
2015-07-23 21:13:35 +0200

    Wrong array index and more...

--- tgbot.php
+++ tgbot.php
@@ -2,60 +2,59 @@
 
 $updid = 0; // The last update_id from the server, this will be changed and used in sequential requests
 
-$token = "put_token_here"; //Your bot's token
+$token = ""; //Your bot's token
 
 while(true){
 
-$a = json_decode(file_get_contents("https://api.telegram.org/bot".$token."/getUpdates?timeout=15&offset=".$updid."&limit=100"),true);
+//	echo "API request...";
 
-$nr = count(@$a["result"]);
+	$a = json_decode(file_get_contents("https://api.telegram.org/bot".$token."/getUpdates?timeout=15&offset=".$updid."&limit=100"),true);
 
+	if($a["ok"] != true){
+		
+		print_r($a);
+		
+	}else{
+		
+		$a = $a["result"];
+		
+		$nr = count($a);
 
-if($a["ok"] != true){
-	
-	print_r($a);
-	
-}else{
-	
-	$a = $a["result"];
-
-if($a != NULL && $nr > 0){
-	
-	$n = 0;
-	
-//	echo "\nHave results:".$nr."\n"; //Debug
-	
-	while($n != ($nr)){
+	if($a != NULL && $nr > 0){
+		
+		$n = 0;
+		
+//		echo "Have results:".$nr; //Debug
+		
+		while($n != ($nr)){
+					
+			$updid = $a[$n]["update_id"]+1;
+			
+			//Some info extracted from the update
+			
+			$to = $a[$n]["message"]["chat"]["id"];
+			
+			$msg = $a[$n]["message"]["text"];
+			
+			//Test feature, respond /ping with /pong
+						
+			if($msg == "/ping"){
 				
-		$updid = $a[$n]["update_id"]+1;
-		
-		//Some info extracted from the update
-		
-		$date = $a[$n]["message"]["date"];
+				$msg_out = urlencode("/pong");
+				
+				file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$to."&text=".$msg_out);
+				
+			}		
+			
+//			echo "."; //Debug
 
-		$to = $a[$n]["message"]["chat"]["id"];
+			$n++;
+				
+			}
 		
-		$msg = $a[$n]["message"];
-		
-		//Test feature, respond /ping with /pong
-		
-		if($msg == "/ping"){
-			
-			$msg_out = urlencode("/pong");
-			
-			file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$to."&text=".$msg_out);
-			
-		}		
-		
-//		echo "."; //Debug
+		}
 
-	$n++;
-		
 	}
-	
-}
-
-}
 
 }
 
